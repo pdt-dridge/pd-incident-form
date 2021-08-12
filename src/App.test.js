2136a89a2477502de79b7d5afd90ac6c7ac0782e
pdt-dridge <dridge@pagerduty.com>
2021-08-12 16:55:55 +1000

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import AlertForm from './AlertForm';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<AlertForm />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Container, Header } from "semantic-ui-react";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import pkg from 'semantic-ui-react/package.json'
+import User from "./User";
+import AlertForm from "./AlertForm";
+
+const Page = ({ children }) => (
+  <Container text style={{ margin: 40 }}>
+    <Header as="h3">This example is powered by Semantic UI React {pkg.version} </Header>
+    {children}
+  </Container>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// TODO: Switch to https://github.com/palmerhq/the-platform#stylesheet when it will be stable
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+document.head.appendChild(styleLink);
+
+ReactDOM.render(
+  <Page>
+    <User />
+  </Page>,
+  document.getElementById("root")
+);
